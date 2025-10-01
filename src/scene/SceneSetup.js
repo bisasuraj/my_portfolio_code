db525ac4eb710fb5db42b62877b3bca3a6cb0c19
Suraj B <surajb@Surajs-MacBook-Pro.local>
2025-10-01 21:54:47 +0100

    mobile aspect ratio bug fix; remove joystick on desktop

--- src/scene/SceneSetup.js
+++ src/scene/SceneSetup.js
@@ -17,10 +17,10 @@ export class SceneSetup {
 
   static createCamera() {
     const fov = 60;
-    const aspect = 1920 / 1080;
+    const aspect = window.innerWidth / window.innerHeight;
     const near = 1.0;
     const far = 1000.0;
-    
+
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.set(25, 10, 10);
 
